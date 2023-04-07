00cf6ebbae3c4cef1513f577096b3becf936991e
JadenKeller <jadkeller@csumb.edu>
2023-04-07 13:09:00 -0700

    init

---
+++ src/test/java/com/cst438/cst438_backend/Cst438BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cst438.cst438_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
